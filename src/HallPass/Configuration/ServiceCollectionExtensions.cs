ebf793fa1567fa8ad8bc71f68542545fbfcf26ea
michael@mharris.io <19492392+Burwin@users.noreply.github.com>
2022-08-17 15:11:20 -0400

    set timeout for httpclient to infinite

--- src/HallPass/Configuration/ServiceCollectionExtensions.cs
+++ src/HallPass/Configuration/ServiceCollectionExtensions.cs
@@ -15,21 +15,16 @@ namespace HallPass
             var options = HallPassOptions.Default;
             config(options);
 
-            // .NET uses a default timeout for HttpClient of 100 seconds. If we need more than that, we'll adjust
-            var defaultMaxTimeout = TimeSpan.FromSeconds(100);
-            var potentialMaxTimeout = options.BucketConfigurationBuilders.Select(b => b.Frequency).Max() * 1.1;
-            var maxTimeout = defaultMaxTimeout > potentialMaxTimeout ? defaultMaxTimeout : potentialMaxTimeout;
-
             // add that handler to the HallPass client's pipeline
             if (options.UseDefaultHttpClient)
             {
                 services
-                    .AddHttpClient(Options.DefaultName, c => c.Timeout = maxTimeout)
+                    .AddHttpClient(Options.DefaultName, c => c.Timeout = System.Threading.Timeout.InfiniteTimeSpan)
                     .AddHttpMessageHandler(serviceProvider => new HallPassMessageHandler(serviceProvider, options));
             }
 
             services
-                .AddHttpClient(Constants.DEFAULT_HALLPASS_HTTPCLIENT_NAME, c => c.Timeout = maxTimeout)
+                .AddHttpClient(Constants.DEFAULT_HALLPASS_HTTPCLIENT_NAME, c => c.Timeout = System.Threading.Timeout.InfiniteTimeSpan)
                 .AddHttpMessageHandler(serviceProvider => new HallPassMessageHandler(serviceProvider, options));
 
             // add a client for calling the HallPass API
